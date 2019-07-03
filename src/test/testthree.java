c1f20a542aacb9683fcf526dca5169b806cdf18c
Saranyatmc1996 <saranyatmc1996@gmail.com>
2019-07-03 09:25:02 +0530

    new code from saran

--- src/test/testthree.java
+++ src/test/testthree.java
@@ -8,6 +8,7 @@ public class testthree {
 		System.out.println(num);
 		System.out.println(num1);
 		System.out.println(num2);
+		System.out.print(num1+num2);
 		for (int i = 0; i <=5; i++) {
 			t=num+num1+num2;
 			System.out.println(t);
