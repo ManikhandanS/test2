27fdb2ac9bcf75c4efac9309ef9766862fe6e12e
ArunmozhiBala <bk.arunmozhi@gmail.com>
2019-07-04 09:45:32 +0530

    added code in test one

--- src/test/testone.java
+++ src/test/testone.java
@@ -8,6 +8,17 @@ public class testone {
 
 	 public static void main(String args[])
 	  {
+		//code added by arunmozhi
+		 int a=10,b=30,c;
+		 if(a==10) {
+			 c=a*b;
+			 System.out.println("a*b ="+c);
+		 }
+			 else {
+				 c=a%b;
+				 System.out.println("a%B"+c);
+		 }
+		// code end by arunmozhi 
 	    int num;
 	    System.out.println("Program");
 	    System.out.println("Enter an Integer number:");
